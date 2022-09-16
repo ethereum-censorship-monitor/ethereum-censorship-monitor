2a07d593fa1166e27e92b7e823a126fdf5675342
Frederik Lührs <luehrs.fred@gmail.com>
2022-09-17 00:44:30 +0200

    Introduce mempool fetcher

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,30 @@
+import time
+
+import click
+from web3 import Web3, HTTPProvider
+
+from mempool import Mempool
 from db.database import Database
 
 SCHEMA_PATH = "./backend/db/data/schema.sql"
 DB_PATH = "./backend/db/data/ethereum-censorship-monitor.db"
 
 
-def main():
-
+def initialize_database():
     database = Database(DB_PATH, SCHEMA_PATH, allow_create=True)
-    database.insert("blocks", {"block_number": 1, "validator": "0xtest"})
+    
+@click.command()
+@click.option("--rpc", type=str, required=True, help="Ethereum RPC endpoint. Required to be a GETH client.")
+def main(rpc):
+    web3 = Web3(HTTPProvider(rpc))
+    mempool = Mempool(web3, 5)
+    mempool.start()
+
+    while True:
+        mempool.process_mempool_results()
+        from pprint import pprint
+        pprint(mempool.transactions)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
