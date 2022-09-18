da8ee10009a3901a8ab7f9736ff0d8a9cd3a4a29
Jannik Luhn <jannik@brainbot.com>
2022-09-18 06:15:42 +0200

    Make websocket connection configurable

--- backend/main.py
+++ backend/main.py
@@ -18,20 +18,21 @@ def initialize_database(db_dir):
 
 @click.command()
 @click.option("--rpc", type=str, required=True, help="Ethereum RPC endpoint. Required to be a GETH client.")
+@click.option("--ws", type=str, required=True, help="Ethereum WS endpoint. Required to be a GETH client.")
 @click.option("--rest-host", type=str, required=True, help="Host of the REST API")
 @click.option("--rest-port", type=int, required=True, help="Port of the REST API")
 @click.option("--db-dir", type=str, required=True, help="Path to the directory containing the db schema and db")
-def main(rpc, rest_host, rest_port, db_dir):
+def main(rpc, ws, rest_host, rest_port, db_dir):
     database = initialize_database(db_dir)
     loop = asyncio.get_event_loop()
-    run_monitor(rpc, database)
+    run_monitor(rpc, ws, database)
     loop.run_until_complete(api.serve(rest_host, rest_port, database))
     loop.run_forever()
 
 
-def run_monitor(rpc, database):
+def run_monitor(rpc, ws, database):
     chain = Chain(database)
-    rpc_client = RPCClient(rpc, "ws://1.geth.mainnet.ethnodes.brainbot.com:8546", chain)
+    rpc_client = RPCClient(rpc, ws, chain)
     rpc_client.start()
 
 
