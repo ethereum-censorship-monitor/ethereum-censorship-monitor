d164fee406baf6e4156369ddb0876e1492fa9d79
Jannik Luhn <jannik.luhn@posteo.de>
2023-01-17 18:02:16 +0100

    Add txs and blocks API endpoints
    These endpoints query the same data as for misses, but group by tx or
    block, respectively. They allow to filter by the minimum number of
    blocks the tx was missed in or txs that were missed in one block.
    
    The response also contains `from` and `to` timestamps that indicate the
    queried range. They can be used for pagination (i.e. use `to` as the
    value for the next `from`).
    
    Grouping is done in Rust instead of SQL for simplicity. Besides
    performance, there is one issue with that: Misses at the boundary of the
    queried interval. A tx that was missed once in the interval, but another
    time outside of the interval will not show up in a query with
    `min_num_misses=2`. In SQL there are solutions for this, but I didn't
    manage to solve this in an efficient manner.

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,7 @@
-use std::cmp::{max, min};
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+};
 
 use actix_web::{
     get,
@@ -31,12 +34,269 @@ pub async fn serve_api(config: Config) -> Result<(), std::io::Error> {
         };
         App::new()
             .app_data(web::Data::new(state))
-            .service(misses)
-            .service(txs)
+            .service(misses_handler)
+            .service(txs_handler)
+            .service(blocks_handler)
     })
     .bind(host_and_port)?
     .run()
     .await
+}
+
+#[get("/v0/misses")]
+async fn misses_handler(
+    data: web::Data<AppState>,
+    q: Query<MissArgs>,
+) -> Result<impl Responder, Error> {
+    let limit = data.config.api_max_response_rows;
+    let misses = query_misses(&q.0, &data.pool, limit).await?;
+    let miss_range = get_miss_range(&misses);
+    let response =
+        ItemizedResponse::new(misses, miss_range, q.0.checked_from()?, q.0.checked_to()?);
+    Ok(Json(response))
+}
+
+#[get("/v0/txs")]
+async fn txs_handler(
+    data: web::Data<AppState>,
+    q: Query<GroupedMissArgs>,
+) -> Result<impl Responder, Error> {
+    let limit = data.config.api_max_response_rows;
+    let min_num_misses = &q.0.checked_min_num_misses()?;
+    let misses_args: &MissArgs = &q.0.into();
+
+    let misses = query_misses(misses_args, &data.pool, limit).await?;
+    let mut txs: Vec<Tx> = group_by_tx(&misses)
+        .values()
+        .filter(|tx| min_num_misses.is_none() || tx.num_misses >= min_num_misses.unwrap())
+        .cloned()
+        .collect();
+    txs.sort_by_key(|tx| tx.blocks[0].proposal_time);
+
+    let miss_range = get_miss_range(&misses);
+    let response = ItemizedResponse::new(
+        txs,
+        miss_range,
+        misses_args.checked_from()?,
+        misses_args.checked_to()?,
+    );
+    Ok(Json(response))
+}
+
+fn group_by_tx(misses: &[Miss]) -> HashMap<&String, Tx> {
+    misses.iter().fold(HashMap::new(), |mut acc, miss| {
+        let mut tx = acc.entry(&miss.tx_hash).or_insert(Tx {
+            tx_hash: miss.tx_hash.clone(),
+            tx_first_seen: miss.tx_first_seen,
+            tx_quorum_reached: miss.tx_quorum_reached,
+            sender: miss.sender.clone(),
+            num_misses: 0,
+            blocks: Vec::new(),
+        });
+        tx.num_misses += 1;
+        tx.blocks.push(MissWithoutTxFields {
+            block_hash: miss.block_hash.clone(),
+            slot: miss.slot,
+            block_number: miss.block_number,
+            proposal_time: miss.proposal_time,
+            proposer_index: miss.proposer_index,
+            tip: miss.tip,
+        });
+        acc
+    })
+}
+
+#[get("/v0/blocks")]
+async fn blocks_handler(
+    data: web::Data<AppState>,
+    q: Query<GroupedMissArgs>,
+) -> Result<impl Responder, Error> {
+    let limit = data.config.api_max_response_rows;
+    let min_num_misses = &q.0.checked_min_num_misses()?;
+    let misses_args: &MissArgs = &q.0.into();
+
+    let misses = query_misses(misses_args, &data.pool, limit).await?;
+    let mut blocks: Vec<Block> = group_by_block(&misses)
+        .values()
+        .filter(|block| min_num_misses.is_none() || block.num_misses >= min_num_misses.unwrap())
+        .cloned()
+        .collect();
+    blocks.sort_by_key(|block| block.proposal_time);
+
+    let miss_range = get_miss_range(&misses);
+    let response = ItemizedResponse::new(
+        blocks,
+        miss_range,
+        misses_args.checked_from()?,
+        misses_args.checked_to()?,
+    );
+    Ok(Json(response))
+}
+
+fn group_by_block(misses: &[Miss]) -> HashMap<&String, Block> {
+    misses.iter().fold(HashMap::new(), |mut acc, miss| {
+        let mut block = acc.entry(&miss.block_hash).or_insert(Block {
+            block_hash: miss.block_hash.clone(),
+            slot: miss.slot,
+            block_number: miss.block_number,
+            proposal_time: miss.proposal_time,
+            proposer_index: miss.proposer_index,
+            num_misses: 0,
+            txs: Vec::new(),
+        });
+        block.num_misses += 1;
+        block.txs.push(MissWithoutBlockFields {
+            tx_hash: miss.tx_hash.clone(),
+            tx_first_seen: miss.tx_first_seen,
+            tx_quorum_reached: miss.tx_quorum_reached,
+            sender: miss.sender.clone(),
+            tip: miss.tip,
+        });
+        acc
+    })
+}
+
+#[derive(Deserialize)]
+struct MissArgs {
+    from: Option<i64>,
+    to: Option<i64>,
+    block_number: Option<i32>,
+    proposer_index: Option<i32>,
+    sender: Option<String>,
+    propagation_time: Option<i64>,
+    min_tip: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+struct Miss {
+    tx_hash: String,
+    block_hash: String,
+    slot: i32,
+    block_number: i32,
+    #[serde(with = "ts_seconds")]
+    proposal_time: NaiveDateTime,
+    proposer_index: i32,
+    #[serde(with = "ts_seconds")]
+    tx_first_seen: NaiveDateTime,
+    #[serde(with = "ts_seconds")]
+    tx_quorum_reached: NaiveDateTime,
+    sender: String,
+    tip: Option<i64>,
+}
+
+#[derive(Deserialize)]
+struct GroupedMissArgs {
+    from: Option<i64>,
+    to: Option<i64>,
+    block_number: Option<i32>,
+    proposer_index: Option<i32>,
+    sender: Option<String>,
+    propagation_time: Option<i64>,
+    min_tip: Option<i64>,
+    min_num_misses: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+struct Tx {
+    tx_hash: String,
+    #[serde(with = "ts_seconds")]
+    tx_first_seen: NaiveDateTime,
+    #[serde(with = "ts_seconds")]
+    tx_quorum_reached: NaiveDateTime,
+    sender: String,
+    num_misses: i64,
+    blocks: Vec<MissWithoutTxFields>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct MissWithoutTxFields {
+    block_hash: String,
+    slot: i32,
+    block_number: i32,
+    #[serde(with = "ts_seconds")]
+    proposal_time: NaiveDateTime,
+    proposer_index: i32,
+    tip: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+struct Block {
+    block_hash: String,
+    slot: i32,
+    block_number: i32,
+    proposal_time: NaiveDateTime,
+    proposer_index: i32,
+    num_misses: i64,
+    txs: Vec<MissWithoutBlockFields>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+struct MissWithoutBlockFields {
+    tx_hash: String,
+    tx_first_seen: NaiveDateTime,
+    tx_quorum_reached: NaiveDateTime,
+    sender: String,
+    tip: Option<i64>,
+}
+
+impl MissArgs {
+    fn checked_from(&self) -> Result<Option<NaiveDateTime>, RequestError> {
+        from_opt_timestamp(self.from, String::from("from"))
+    }
+
+    fn checked_to(&self) -> Result<Option<NaiveDateTime>, RequestError> {
+        from_opt_timestamp(self.to, String::from("to"))
+    }
+
+    fn checked_time_range(
+        &self,
+    ) -> Result<(Option<NaiveDateTime>, Option<NaiveDateTime>), RequestError> {
+        Ok(ordered_timestamps(self.checked_from()?, self.checked_to()?))
+    }
+
+    fn checked_is_order_ascending(&self) -> Result<bool, RequestError> {
+        Ok(is_order_ascending(self.checked_from()?, self.checked_to()?))
+    }
+
+    fn checked_block_number(&self) -> Result<Option<i32>, RequestError> {
+        from_opt_nonneg_uint(self.block_number, String::from("block_number"))
+    }
+
+    fn checked_proposer_index(&self) -> Result<Option<i32>, RequestError> {
+        from_opt_nonneg_uint(self.proposer_index, String::from("proposer_index"))
+    }
+
+    fn checked_sender(&self) -> Result<Option<&String>, RequestError> {
+        Ok(self.sender.as_ref())
+    }
+
+    fn checked_propagation_time(&self) -> Result<Option<PgInterval>, RequestError> {
+        from_opt_interval(self.propagation_time, String::from("propagation_time"))
+    }
+
+    fn checked_min_tip(&self) -> Result<Option<i64>, RequestError> {
+        from_opt_nonneg_uint(self.min_tip, String::from("min_tip"))
+    }
+}
+
+impl GroupedMissArgs {
+    fn checked_min_num_misses(&self) -> Result<Option<i64>, RequestError> {
+        from_opt_nonneg_uint(self.min_num_misses, String::from("min_num_misses"))
+    }
+}
+
+impl From<GroupedMissArgs> for MissArgs {
+    fn from(m: GroupedMissArgs) -> Self {
+        Self {
+            from: m.from,
+            to: m.to,
+            block_number: m.block_number,
+            proposer_index: m.proposer_index,
+            sender: m.sender,
+            propagation_time: m.propagation_time,
+            min_tip: m.min_tip,
+        }
+    }
 }
 
 #[derive(Debug, Error)]
@@ -60,261 +320,52 @@ impl ResponseError for RequestError {
 #[derive(Debug, Serialize)]
 pub struct ItemizedResponse<T> {
     items: Vec<T>,
-    complete: bool,
+    #[serde(with = "ts_seconds_option")]
+    from: Option<NaiveDateTime>,
+    #[serde(with = "ts_seconds_option")]
+    to: Option<NaiveDateTime>,
 }
 
 impl<T> ItemizedResponse<T> {
-    fn new(mut items: Vec<T>, limit: usize) -> Self {
-        let complete = items.len() <= limit;
-        items.truncate(limit);
-        Self { items, complete }
+    fn new(
+        items: Vec<T>,
+        data_range: Option<(NaiveDateTime, NaiveDateTime)>,
+        query_from: Option<NaiveDateTime>,
+        query_to: Option<NaiveDateTime>,
+    ) -> Self {
+        let (from, to) = if let Some((from, to)) = data_range {
+            (Some(from), Some(to))
+        } else {
+            (query_from, query_to)
+        };
+        Self { items, from, to }
     }
 }
 
-#[derive(Debug, Serialize)]
-pub struct Miss {
-    tx_hash: String,
-    block_hash: String,
-    slot: i32,
-    block_number: i32,
-    #[serde(with = "ts_seconds")]
-    proposal_time: NaiveDateTime,
-    proposer_index: i32,
-    #[serde(with = "ts_seconds")]
-    tx_first_seen: NaiveDateTime,
-    #[serde(with = "ts_seconds")]
-    tx_quorum_reached: NaiveDateTime,
-    sender: String,
-    tip: Option<i64>,
-}
-
-#[derive(Deserialize)]
-struct MissesArgs {
-    from: Option<i64>,
-    to: Option<i64>,
-    block_number: Option<i32>,
-    proposer_index: Option<i32>,
-    sender: Option<String>,
-    propagation_time: Option<i64>,
-    min_tip: Option<i64>,
-}
-
-#[get("/v0/misses")]
-async fn misses(data: web::Data<AppState>, q: Query<MissesArgs>) -> Result<impl Responder, Error> {
-    let from = from_opt_timestamp(q.from, String::from("from"))?;
-    let to = from_opt_timestamp(q.to, String::from("to"))?;
-    let order_ascending = if from.is_none() || to.is_none() {
-        true
-    } else {
-        from.unwrap() <= to.unwrap()
-    };
-    let (tmin, tmax) = ordered_timestamps(from, to);
-
-    let block_number = from_opt_nonneg_uint(q.block_number, String::from("block_number"))?;
-    let proposer_index = from_opt_nonneg_uint(q.proposer_index, String::from("proposer_index"))?;
-    let sender = q.sender.clone();
-
-    let propagation_time = from_opt_interval(q.propagation_time, String::from("propagation_time"))?;
-
-    let min_tip = from_opt_nonneg_uint(q.min_tip, String::from("min_tip"))?;
-
-    let limit = data.config.api_max_response_rows;
-
-    let result = sqlx::query_as!(
+async fn query_misses(args: &MissArgs, pool: &db::Pool, limit: usize) -> Result<Vec<Miss>, Error> {
+    let result = sqlx::query_file_as!(
         Miss,
-        r#"
-        SELECT
-            tx_hash,
-            block_hash,
-            slot,
-            block_number,
-            proposal_time,
-            proposer_index,
-            tx_first_seen,
-            tx_quorum_reached,
-            sender,
-            tip
-        FROM
-            data.full_miss
-        WHERE
-            ($1::timestamp IS NULL OR proposal_time >= $1) AND
-            ($2::timestamp IS NULL OR proposal_time <= $2) AND
-            ($3::integer IS NULL OR block_number = $3) AND
-            ($4::integer IS NULL OR proposer_index = $4) AND
-            ($5::char(42) IS NULL OR sender = $5) AND
-            ($6::interval IS NULL OR proposal_time - tx_quorum_reached > $6) AND
-            ($7::bigint IS NULL OR tip >= $7)
-        ORDER BY
-            CASE WHEN $8 THEN
-                proposal_time
-            ELSE
-                to_timestamp(0)
-            END ASC,
-            CASE WHEN $8 THEN
-                tx_first_seen
-            ELSE
-                to_timestamp(0)
-            END ASC,
-            CASE WHEN $8 THEN
-                to_timestamp(0)
-            ELSE
-                proposal_time
-            END DESC,
-            CASE WHEN $8 THEN
-                to_timestamp(0)
-            ELSE
-                tx_first_seen
-            END DESC
-        LIMIT $9
-        "#,
-        tmin,
-        tmax,
-        block_number,
-        proposer_index,
-        sender,
-        propagation_time,
-        min_tip,
-        order_ascending,
+        "src/misses_query.sql",
+        args.checked_time_range()?.0,
+        args.checked_time_range()?.1,
+        args.checked_block_number()?,
+        args.checked_proposer_index()?,
+        args.checked_sender()?,
+        args.checked_propagation_time()?,
+        args.checked_min_tip()?,
+        args.checked_is_order_ascending()?,
         (limit + 1) as i64,
     )
-    .fetch_all(&data.pool)
+    .fetch_all(pool)
     .await;
 
-    if let Err(e) = result {
-        log::error!("error fetching misses from db: {}", e);
-        return Err(Error::from(InternalError {}));
+    match result {
+        Err(e) => {
+            log::error!("error fetching txs from db: {}", e);
+            Err(Error::from(InternalError {}))
+        }
+        Ok(v) => Ok(v),
     }
-    let response = ItemizedResponse::new(result.unwrap(), limit);
-    Ok(Json(response))
-}
-
-#[derive(Debug, Serialize)]
-pub struct Tx {
-    tx_hash: String,
-    #[serde(with = "ts_seconds_option")]
-    tx_first_seen: Option<NaiveDateTime>,
-    #[serde(with = "ts_seconds_option")]
-    tx_quorum_reached: Option<NaiveDateTime>,
-    sender: Option<String>,
-    num_misses: Option<i64>,
-}
-
-#[derive(Deserialize)]
-struct TxsArgs {
-    from: Option<i64>,
-    to: Option<i64>,
-    block_number: Option<i32>,
-    proposer_index: Option<i32>,
-    sender: Option<String>,
-    propagation_time: Option<i64>,
-    min_tip: Option<i64>,
-    min_num_misses: Option<i32>,
-}
-
-#[get("/v0/txs")]
-async fn txs(data: web::Data<AppState>, q: Query<TxsArgs>) -> Result<impl Responder, Error> {
-    let from = from_opt_timestamp(q.from, String::from("from"))?;
-    let to = from_opt_timestamp(q.to, String::from("to"))?;
-    let order_ascending = if from.is_none() || to.is_none() {
-        true
-    } else {
-        from.unwrap() <= to.unwrap()
-    };
-    let (tmin, tmax) = ordered_timestamps(from, to);
-
-    let block_number = from_opt_nonneg_uint(q.block_number, String::from("block_number"))?;
-    let proposer_index = from_opt_nonneg_uint(q.proposer_index, String::from("proposer_index"))?;
-    let sender = q.sender.clone();
-
-    let propagation_time = from_opt_interval(q.propagation_time, String::from("propagation_time"))?;
-
-    let min_tip = from_opt_nonneg_uint(q.min_tip, String::from("min_tip"))?;
-
-    let limit = data.config.api_max_response_rows;
-
-    let min_num_misses = from_opt_nonneg_uint(q.min_num_misses, String::from("min_num_misses"))?;
-
-    let result = sqlx::query_as!(
-        Tx,
-        r#"
-        SELECT
-            *
-        FROM (
-            SELECT
-                tx.tx_hash AS tx_hash,
-                min(tx.tx_first_seen) AS tx_first_seen,
-                min(full_miss.tx_quorum_reached) AS tx_quorum_reached,
-                min(full_miss.sender) AS sender,
-                COALESCE(count(full_miss.block_hash), 0) AS num_misses
-            FROM (
-                SELECT
-                    tx_hash,
-                    proposal_time,
-                    block_number,
-                    tx_first_seen,
-                    tx_quorum_reached,
-                    sender
-                FROM
-                    data.full_miss
-                WHERE
-                    ($1::timestamp IS NULL OR proposal_time >= $1) AND
-                    ($2::timestamp IS NULL OR proposal_time <= $2) AND
-                    ($3::integer IS NULL OR block_number = $3) AND
-                    ($4::integer IS NULL OR proposer_index = $4) AND
-                    ($5::char(42) IS NULL OR sender = $5) AND
-                    ($6::interval IS NULL OR proposal_time - tx_quorum_reached > $6) AND
-                    ($7::bigint IS NULL OR tip >= $7)
-                ORDER BY
-                    CASE WHEN $8 THEN
-                        proposal_time
-                    ELSE
-                        to_timestamp(0)
-                    END ASC,
-                    CASE WHEN $8 THEN
-                        tx_first_seen
-                    ELSE
-                        to_timestamp(0)
-                    END ASC,
-                    CASE WHEN $8 THEN
-                        to_timestamp(0)
-                    ELSE
-                        proposal_time
-                    END DESC,
-                    CASE WHEN $8 THEN
-                        to_timestamp(0)
-                    ELSE
-                        tx_first_seen
-                    END DESC
-                LIMIT $9
-            ) AS tx
-            INNER JOIN data.full_miss ON full_miss.tx_hash = tx.tx_hash
-            WHERE
-                ($6::interval IS NULL OR full_miss.proposal_time - full_miss.tx_quorum_reached > $6) AND
-                ($7::bigint IS NULL OR full_miss.tip >= $7)
-            GROUP BY tx.tx_hash
-        ) AS tx
-        WHERE ($10::integer IS NULL OR num_misses >= $10)
-        "#,
-        tmin,
-        tmax,
-        block_number,
-        proposer_index,
-        sender,
-        propagation_time,
-        min_tip,
-        order_ascending,
-        (limit + 1) as i64,
-        min_num_misses
-    )
-    .fetch_all(&data.pool)
-    .await;
-
-    if let Err(e) = result {
-        log::error!("error fetching txs from db: {}", e);
-        return Err(Error::from(InternalError {}));
-    }
-    let response = ItemizedResponse::new(result.unwrap(), limit);
-    Ok(Json(response))
 }
 
 fn from_opt_timestamp(
@@ -380,5 +431,24 @@ fn ordered_timestamps(
         (None, _) => (from, to),
         (_, None) => (from, to),
         (Some(from), Some(to)) => (Some(min(from, to)), Some(max(from, to))),
+    }
+}
+
+fn is_order_ascending(t1: Option<NaiveDateTime>, t2: Option<NaiveDateTime>) -> bool {
+    match (t1, t2) {
+        (None, _) => true,
+        (_, None) => true,
+        (Some(from), Some(to)) => from <= to,
+    }
+}
+
+fn get_miss_range(misses: &Vec<Miss>) -> Option<(NaiveDateTime, NaiveDateTime)> {
+    if misses.is_empty() {
+        None
+    } else {
+        Some((
+            misses[0].proposal_time,
+            misses[misses.len() - 1].proposal_time,
+        ))
     }
 }
---
+++ src/misses_query.sql
@@ -0,0 +1,53 @@
+-- parameters:
+-- $1: tmin
+-- $2: tmax
+-- $3: block_number
+-- $4: proposer_index
+-- $5: sender
+-- $6: min_propagation_time
+-- $7: min_tip
+-- $8: is_order_ascending
+
+SELECT
+    tx_hash,
+    block_hash,
+    slot,
+    block_number,
+    proposal_time,
+    proposer_index,
+    tx_first_seen,
+    tx_quorum_reached,
+    sender,
+    tip
+FROM
+    data.full_miss
+WHERE
+    ($1::timestamp IS NULL OR proposal_time >= $1) AND
+    ($2::timestamp IS NULL OR proposal_time <= $2) AND
+    ($3::integer IS NULL OR block_number = $3) AND
+    ($4::integer IS NULL OR proposer_index = $4) AND
+    ($5::char(42) IS NULL OR sender = $5) AND
+    ($6::interval IS NULL OR proposal_time - tx_quorum_reached > $6) AND
+    ($7::bigint IS NULL OR tip >= $7)
+ORDER BY
+    CASE WHEN $8 THEN
+        proposal_time
+    ELSE
+        to_timestamp(0)
+    END ASC,
+    CASE WHEN $8 THEN
+        tx_first_seen
+    ELSE
+        to_timestamp(0)
+    END ASC,
+    CASE WHEN $8 THEN
+        to_timestamp(0)
+    ELSE
+        proposal_time
+    END DESC,
+    CASE WHEN $8 THEN
+        to_timestamp(0)
+    ELSE
+        tx_first_seen
+    END DESC
+LIMIT $9;
